b3c32e7882155247d225d786986f0e291dd3c4d2
Wesley <wspencer3295@yahoo.com>
2021-01-22 15:08:28 -0700

    +helper methods for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+        !!current_user
+      end
+  
+      def logout
+        session.clear
+      end
+  
+    #   def redirect_if_logged_in
+    #     redirect '/teams' if logged_in?
+    #   end
 end
-# <%= javascript_pack_tag 'application', 'data-turbolinks-track': 'reload' %>
