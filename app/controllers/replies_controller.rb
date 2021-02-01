12aae0e08d9242aeaeb80d5f17d517b2560856c3
Wesley <wspencer3295@yahoo.com>
2021-01-31 22:11:35 -0700

    refactor

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,23 +1,24 @@
 class RepliesController < ApplicationController
+    before_action :find_reply, only: [:destroy,:update,:edit]
+    before_action :bad_sources, only: [:create]
+
     def create
         @reply = Reply.new(reply_params)
         @comment = @reply.comment
         @article = @comment.article
-       @reply.user_id = current_user.id
+        @reply.user_id = current_user.id
         if @reply.save  
         redirect_to article_comments_path(@article)#route to art/id/com/id
         else
-             render  :edit
+             render '/comments/index'
         end
     end
 
     def edit
-        find_reply
         @comment = @reply.comment
     end
 
     def update
-        find_reply
         article = @reply.comment.article
         if @reply.update(reply_params)
             redirect_to article_comments_path(article)
@@ -27,7 +28,6 @@ class RepliesController < ApplicationController
     end
 
     def destroy
-        find_reply
         article = @reply.comment.article
         @reply.delete
         redirect_to article_comments_path(article)
@@ -35,10 +35,16 @@ class RepliesController < ApplicationController
 
 
     private
+
     def find_reply
         @reply = Reply.find_by_id(params[:id])
     end
+
     def reply_params
         params.require(:reply).permit(:content, :comment_id)
+    end
+
+    def bad_sources
+        @bad_sources = ["CNBC","CNN Europe","Engadget","Fansided","Financial Post | Canada Business News","Forbes","Seeking Alpha","Sports Illustrated","Sports | Reddit", "The Verge","Yardbarker.com","business","dailynorthwestern","dennews","deseretnews","euroweeklynews","hitfix","indybay","nationalpost","newswithviews","si","stylecaster","suntimes","thestar","tmz","torontosun","tribune242","vancouversun","wwd"]
     end
 end
